32b19e496fb8d83aab3f3ca1e0f8b250d0fd87ee
cosmin <cosmin.firta@visma.com>
2018-09-09 00:49:27 +0300

    step1 - added the test

--- app/src/androidTest/java/com/economicvisma/tddmyfriends/MainActivityTest.kt
+++ app/src/androidTest/java/com/economicvisma/tddmyfriends/MainActivityTest.kt
@@ -1,10 +1,14 @@
 package com.economicvisma.tddmyfriends
 
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import com.economicvisma.tddmyfriends.helper.RecyclerViewItemCountAssertion
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 
@@ -14,9 +18,16 @@ class MainActivityTest {
     @get:Rule
     public val mActivityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
 
+    private val totalFriendCount = 10
 
     @Before
     fun init() {
+    }
+
+    @Test
+    fun scenario1_checkIfListHasFriends() {
+        Espresso.onView(ViewMatchers.withId(R.id.friend_list))
+            .check(RecyclerViewItemCountAssertion(totalFriendCount))
     }
 
 }
