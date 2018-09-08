59b247a8c7456172a91b098c77406c364a862adc
cosmin <cosmin.firta@visma.com>
2018-09-08 23:55:11 +0300

    added the helper for counting items in recycler view and test class for the mainActivity

---
+++ app/src/androidTest/java/com/economicvisma/tddmyfriends/MainActivityTest.kt
@@ -0,0 +1,21 @@
+package com.economicvisma.tddmyfriends
+
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityTest {
+    @Rule
+    var mActivityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    @Before
+    fun init() {
+    }
+
+}
---
+++ app/src/androidTest/java/com/economicvisma/tddmyfriends/helper/RecyclerViewItemCountAssertion.kt
@@ -0,0 +1,20 @@
+package com.economicvisma.tddmyfriends.helper
+
+import android.support.v7.widget.RecyclerView
+import android.support.test.espresso.NoMatchingViewException
+import android.support.test.espresso.ViewAssertion
+import android.support.test.espresso.matcher.ViewMatchers.assertThat
+import android.view.View
+import org.hamcrest.CoreMatchers.`is`
+
+
+class RecyclerViewItemCountAssertion(private val expectedCount: Int) : ViewAssertion {
+    override fun check(view: View, noViewFoundException: NoMatchingViewException?) {
+        if (noViewFoundException != null) {
+            throw noViewFoundException
+        }
+        val recyclerView = view as RecyclerView
+        val adapter = recyclerView.adapter
+        assertThat(adapter!!.itemCount, `is`(expectedCount))
+    }
+}
