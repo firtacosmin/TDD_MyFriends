afd95e650d853f6aba0b4e7369b36c4734e2f227
cosmin <cosmin.firta@visma.com>
2018-09-09 01:30:45 +0300

    linked the presenter with the adapter to get the total count

--- app/src/main/java/com/economicvisma/tddmyfriends/FriendsRecViewAdapter.kt
+++ app/src/main/java/com/economicvisma/tddmyfriends/FriendsRecViewAdapter.kt
@@ -4,14 +4,16 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 
-class FriendsRecViewAdapter : RecyclerView.Adapter<FriendsViewHolder>() {
+class FriendsRecViewAdapter(
+    private val presenter: FriendsPresenter
+) : RecyclerView.Adapter<FriendsViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, position: Int): FriendsViewHolder {
         return FriendsViewHolder(View(parent.context))
     }
 
     override fun getItemCount(): Int {
-        return 3
+        return presenter.totalFriends
     }
 
     override fun onBindViewHolder(holder: FriendsViewHolder, position: Int) {
